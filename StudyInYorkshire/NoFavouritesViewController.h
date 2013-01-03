6ca50f39d6b2edb69bf7bf32322b269dba436449
Matt Atkins <matt@yoomee.com>
2013-01-03 16:13:52 +0000

    Add launch images

---
+++ StudyInYorkshire/NoFavouritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoFavouritesViewController.h
+//  StudyInYorkshire
+//
+//  Created by Matthew Atkins on 03/01/2013.
+//  Copyright (c) 2013 Yoomee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoFavouritesViewController : UIViewController
+
+@end
