28761e5fb8ee2f581055ad2594a6b04795343edd
Matt Atkins <matt@yoomee.com>
2013-01-29 10:01:01 +0000

    Don't use retina page images

--- StudyInYorkshire/Page.m
+++ StudyInYorkshire/Page.m
@@ -97,8 +97,6 @@
         NSString *path = [[NSString stringWithFormat:@"%@/assets/%@",[[NSBundle mainBundle] resourcePath],self.imageUID] stringByDeletingPathExtension];
         if(iPad)
             path = [path stringByAppendingString:@"~ipad"];
-        if([UIScreen mainScreen].scale == 2.0)
-            path = [path stringByAppendingString:@"@2x"];
         path = [path stringByAppendingString:@".jpg"];
         NSLog(@"%@",path);
         return [UIImage imageWithContentsOfFile:path];
